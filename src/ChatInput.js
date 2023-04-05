f1e56f445572df08a6f8f956cf77c944f236d169
Andrei <andrei.borovan@icloud.com>
2023-04-05 23:59:48 +0300

    ChatInput+App

---
+++ src/ChatInput.js
@@ -0,0 +1,13 @@
+function ChatInput({sendToOpenAi, setPrompt}){
+    function submit(e){
+        e.preventDefault
+    }
+    return(
+        <div className="chat-input">
+            <form className="chat-container" onSubmit={(e) => submit(e)}>
+                <input type="text" placeholder="Please enter your message here" id="user-input" onChange={(e) => sendToOpenAi(e.target.value)} />
+                <button type="submit" onClick={setPrompt}>Send</button>
+            </form>
+        </div>
+    )
+}
