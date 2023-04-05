5bf3a152307ad18426f67aa2c483786b74785605
Crina <crinna042@gmail.com>
2023-04-05 17:55:39 +0300

    1st try Crina

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="header">
+            <h1>CopyGPT</h1>
+            <p>Here to entertain you!</p>
+        </div>
+    )
+}
+
+export default Header;
