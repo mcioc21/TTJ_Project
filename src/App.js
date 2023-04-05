9f1b3d960c02bdd46cb94ce9cfde7e0cc43042aa
Matei <matei.ciocanescu10@gmail.com>
2023-04-05 22:31:55 +0300

    Added TypedReply.js, needs to be implemented in ChatHistory.js when added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import TypedReply from './TypedReply';
+
 function App() {
   return (
     <div className="App">
---
+++ src/TypedReply.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const TypedReply = ({content = "", speed = 1200}) => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        console.log('useEffect animKey', content, speed);
+        setIndex(0);
+        const animKey = setInterval(() => {
+            setIndex((index) => {
+                console.log('index', index);
+                if(index >= content.length - 1) {
+                    clearInterval(animKey);
+                    return index;
+                }
+                return index+1;
+            });
+        }, speed);
+    }, {content, speed});
+
+    let displayedContent = (content, index) => {
+        if(index >= content.length - 1 ) {
+            return content;
+        }
+        return content.slice(0,index) + "█";
+    }
+    return <pre className="type-writer">{displayedContent(content, index)}</pre>
+}
